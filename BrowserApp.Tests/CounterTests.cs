568d4ff9dfa44bcf18961b80f3a33d8a3c1cf41b
Jeroen Bos <jer123bos@gmail.com>
2018-09-16 14:47:23 +0200

    [OK] Test counter.increment

---
+++ BrowserApp.Tests/CounterTests.cs
@@ -0,0 +1,51 @@
+﻿using BrowserApp.Commands;
+using BrowserApp.POCOs;
+using JBSnorro.Diagnostics;
+using JBSnorro.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrowserApp.Tests
+
+{
+    [TestClass]
+    public class CounterTests
+    {
+        SpecificAppViewModel root;
+        List<Change> changes;
+        View view;
+        Logger logger;
+        IIdProvider idProvider;
+        CommandManager commandManager => root.CommandManager;
+        CounterViewModel counter => root.Counter;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.root = SpecificCode.getRoot_TODO_ToBeProvidedByExtension(null);
+            SpecificCode.Initialize(commandManager);
+            this.logger = new Logger();
+            this.changes = new List<Change>();
+            this.idProvider = new IdProvider();
+            this.view = new View(root, changes.Add, this.idProvider);
+        }
+
+        public CounterTests() => Initialize();
+        [TestMethod]
+        public void TestExecuteIncrementCommandResponse()
+        {
+            Contract.Assert(this.changes.Count == 0);
+
+            this.commandManager.Execute(null, "Increment", counter, new object());
+
+            Assert.AreEqual(1, this.changes.Count);
+            Assert.IsInstanceOfType(this.changes[0], typeof(PropertyChange));
+            var change = this.changes[0] as PropertyChange;
+            Assert.AreEqual(idProvider[counter], change.Id);
+            Assert.AreEqual("currentCount", change.PropertyName);
+
+        }
+    }
+}
