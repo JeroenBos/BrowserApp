2f148f214e9e1454f4889c3918766a7fea670c5f
Jeroen Bos <jer123bos@gmail.com>
2018-09-17 20:12:25 +0200

    Add ExceptionFilter for debugging

---
+++ JBSnorro.View.App/ExceptionFilter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace JBSnorro.View.App
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+
+        }
+    }
+}
